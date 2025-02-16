aab50424eb3142096cf151a57458ffea083e6e73
Zay Yar Phyo <streammineking@gmail.com>
2025-02-16 11:33:00 +0630

    Feb 16

--- RenonationDay3.java
+++ RenonationDay3.java
@@ -2,14 +2,13 @@ package com.example;
 import java.util.Random;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
-class RenonationDay2 {
+class RenonationDay4 {
     static WebDriver driver = new EdgeDriver();
-
 
     void setUp(){
         driver.get("https://web-staging.renonation.sg/");
@@ -109,12 +108,105 @@ class RenonationDay2 {
             driver.findElement(By.xpath("//*[@id=\"submitBtnPortal\"]/button")).click();
         }
 
+        void stepThree() throws InterruptedException{
+            Thread.sleep(2000);
+
+            driver.findElement(By.xpath("//*[@id=\"preferredStyles\"]/div/div[2]/div/div[1]/div/ul/li[2]/div[1]/div/img")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@id=\"submitBtnPortal\"]/button")).click();
+            System.out.println("Step Three Success");
+        }
+
+        void logOut() throws InterruptedException{
+            Thread.sleep(3000);
+            //logout icon
+            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[2]/div/header/div/div[2]/div/div[2]/div/div/div")).click();
+            Thread.sleep(3000); 
+
+            //Logout link
+            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div[2]/div/header/div/div[2]/div/div[2]/div/div[2]/div")).click();
+            Thread.sleep(3000); 
+
+            //Logout Button
+            driver.findElement(By.xpath("//*[@id=\"radix-:rk:\"]/form/div/div/button[2]")).click();
+            Thread.sleep(2000);
+        }
+        void search() throws InterruptedException{
+            Thread.sleep(3000);
+            WebElement element = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[4]/section/div[2]/div/a/div[2]"));
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", element);
+            Thread.sleep(1000);
+
+            driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[4]/section/div[3]/a[2]")).click();
+            Thread.sleep(2000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[2]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[3]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[4]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[5]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[6]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[7]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[8]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[9]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[10]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[11]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[12]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[13]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[14]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[15]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[16]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[17]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[18]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[19]/img")));
+            Thread.sleep(1000);
+
+            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath("//*[@id=\"__next\"]/div/main/div[1]/section/div[2]/div[1]/div[2]/figure[20]/img")));
+           
+            
+        }
         public static void main(String[] args)throws InterruptedException {
         RenonationDay2 r = new RenonationDay2();
         r.setUp();
-        r.singUp();
-        r.stepOne();
-        r.stepTwo();
+        // r.singUp();
+        // r.stepOne();
+        // r.stepTwo();
+        // r.stepThree();
+        // r.logOut();
+        r.search();
         Thread.sleep(5000);
         driver.quit();
     }
